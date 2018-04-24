88f3e9f2a0b9c0a9ced5ac853de829e2f8e28c7f
shuxf <shuxianfon1987@126.com>
2018-04-24 11:58:22 +0800

    注释无用代码

--- springboot-quartz/src/main/java/com/zyd/quartz/job/Minute2Job.java
+++ springboot-quartz/src/main/java/com/zyd/quartz/job/Minute2Job.java
@@ -10,12 +10,12 @@ import org.slf4j.LoggerFactory;
  * @Author shuxf
  * @Date 2018/4/23 17:11
  */
-public class Minute2Job implements Job {
-
-    private static final Logger logger = LoggerFactory.getLogger(Minute2Job.class);
-
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        logger.info("JobName2: {}", context.getJobDetail().getKey().getName());
-    }
-}
+//public class Minute2Job implements Job {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(Minute2Job.class);
+//
+//    @Override
+//    public void execute(JobExecutionContext context) throws JobExecutionException {
+//        logger.info("JobName2: {}", context.getJobDetail().getKey().getName());
+//    }
+//}
--- springboot-quartz/src/main/java/com/zyd/quartz/job/MinuteJob.java
+++ springboot-quartz/src/main/java/com/zyd/quartz/job/MinuteJob.java
@@ -11,14 +11,14 @@ import org.springframework.stereotype.Service;
  * @Author shuxf
  * @Date 2018/4/19 17:08
  */
-@SuppressWarnings("ALL")
-@Service
-public class MinuteJob implements Job {
-
-    private static final Logger logger = LoggerFactory.getLogger(MinuteJob.class);
-
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        logger.info("JobName: {}", context.getJobDetail().getKey().getName());
-    }
-}
+//@SuppressWarnings("ALL")
+//@Service
+//public class MinuteJob implements Job {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(MinuteJob.class);
+//
+//    @Override
+//    public void execute(JobExecutionContext context) throws JobExecutionException {
+//        logger.info("JobName: {}", context.getJobDetail().getKey().getName());
+//    }
+//}
