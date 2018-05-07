7856c2d236cc4eacc256a3190603c2cb75285550
shuxf <shuxianfon1987@126.com>
2018-05-07 09:15:49 +0800

    完善注解

--- springboot-blog/src/main/java/com/sxf/blog/mapper/BlogMapper.java
+++ springboot-blog/src/main/java/com/sxf/blog/mapper/BlogMapper.java
@@ -2,8 +2,11 @@ package com.sxf.blog.mapper;
 
 import com.sxf.blog.entity.Blog;
 import com.sxf.blog.entity.UserVo;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface BlogMapper {
     int deleteByPrimaryKey(Integer id);
 
